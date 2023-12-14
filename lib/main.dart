8ec43b402a4a398701b0f922cfa587a66c805b75
Mahdi Ahbabi <mahdi.m12528@yahoo.com>
2023-12-14 14:00:00 +0330

    Auth Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_shop/mainwarpper.dart';
 import 'package:ecommerce_shop/theme/theme.dart';
+import 'package:ecommerce_shop/ui/auth/auth.dart';
 import 'package:ecommerce_shop/ui/home_screen/home.dart';
 import 'package:flutter/material.dart';
 
@@ -16,13 +17,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         textButtonTheme: const TextButtonThemeData(
-          style: ButtonStyle(
-            textStyle: MaterialStatePropertyAll(TextStyle(color: Color.fromARGB(255, 143, 149, 158))),
-            // backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 63, 125, 218))
-          )
-        ),
+            style: ButtonStyle(
+          textStyle: MaterialStatePropertyAll(
+              TextStyle(color: Color.fromARGB(255, 143, 149, 158))),
+          // backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 63, 125, 218))
+        )),
         textTheme: const TextTheme(
             titleLarge: TextStyle(
               fontSize: 28,
@@ -30,19 +30,23 @@ class MyApp extends StatelessWidget {
               color: LightColor.titleTextColor,
             ),
             bodyMedium: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
-              color: Color.fromARGB(255, 29, 30, 32)
-            ),
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color.fromARGB(255, 29, 30, 32)),
             bodySmall: TextStyle(
               fontSize: 15,
               color: LightColor.captionTextColor,
             )),
         colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.black, background: LightColor.backgroundColor, secondary: Colors.blue),
+            seedColor: Colors.black,
+            background: LightColor.backgroundColor,
+            secondary: Colors.blue),
         useMaterial3: true,
       ),
-      home:  MainWarpper(),
+      home: 
+      // MainWarpper(),
+       AuthScreen()
+      // MainWarpper(),
     );
   }
 }
--- lib/ui/auth/auth.dart
+++ lib/ui/auth/auth.dart
@@ -1,18 +1,125 @@
+import 'package:ecommerce_shop/ui/auth/bloc/auth_bloc.dart';
+import 'package:fk_toggle/fk_toggle.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthScreen extends StatelessWidget {
-  const AuthScreen({super.key});
-
+  AuthScreen({super.key});
+  TextEditingController textEditingControllerUserName = TextEditingController();
+  TextEditingController textEditingControllerPassword = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: [
-         
-        ],
-      )),
+    return BlocProvider(create: (context) {
+      final authBloc = AuthBloc();
+      authBloc.add(AuthStarted(0));
+      return authBloc;
+    }, child: BlocBuilder<AuthBloc, AuthState>(
+      builder: (BuildContext context, state) {
+        return Scaffold(
+          body: SafeArea(
+              child: Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: Column(children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+              Text('welcome'),
+              Text('please enter your data to continue'),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 5,
+              ),
+              AuthTextField(
+                icon: Icon(Icons.task_alt_outlined),
+                lable: 'UserName',
+                suffixText: '',
+                textEditingController: textEditingControllerUserName,
+                isUsername: true,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              AuthTextField(
+                  textEditingController: textEditingControllerPassword,
+                  icon: Icon(Icons.stacked_bar_chart),
+                  lable: 'Password',
+                  suffixText: 'Strong',
+                  isUsername: false),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  //TODO  فاصله متن با ساختار کلی متفاوت است
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text('Forgot your password ?')),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 3, right: 3),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Remember me'),
+                    FkToggle(
+                      width: 30,
+                      height: 30,
+                      labels: const ['no', 'yes'],
+                      backgroundColor: Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'By connecting your account confirm that you agree with our Term and Condition',
+                textAlign: TextAlign.center,
+              )
+            ]),
+          )),
+        );
+      },
+    ));
+  }
+}
+
+class AuthTextField extends StatefulWidget {
+  const AuthTextField({
+    super.key,
+    required this.textEditingController,
+    required this.icon,
+    required this.lable,
+    required this.suffixText,
+    required this.isUsername,
+  });
+  final TextEditingController textEditingController;
+  final Icon icon;
+  final String lable;
+  final String suffixText;
+  final bool isUsername;
+
+  @override
+  State<AuthTextField> createState() => _AuthTextFieldState();
+}
+
+class _AuthTextFieldState extends State<AuthTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      //TODO : prifix and suffix dont work
+      controller: widget.textEditingController,
+
+      decoration: InputDecoration(
+          suffixIcon:
+              widget.isUsername && widget.textEditingController.text.isNotEmpty
+                  ? widget.icon
+                  : null,
+          suffixIconColor: Colors.green,
+          labelText: widget.lable,
+          suffixText: widget.textEditingController.text.isNotEmpty
+              ? widget.suffixText
+              : '',
+          suffixStyle: TextStyle(color: Colors.green),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(35))),
     );
   }
 }
